998db2f24feced8acb586e95615ce17a85bf468c
Andrew Kim <jeesoo2002@gmail.com>
2020-10-21 20:58:11 +0900

    installed filesystem and transformer plugin to gatsby

---
+++ src/pages/templates/post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+export default function Template({data}) {
+    const {markdownRemark: post} = data;
+    return (
+        <div>
+            <h1>{post.frontmatter.title}</h1>
+        </div>
+    )
+}
+
+// export const postQuery = graphql`
+//     query BlogPostByPath($path: String!)
+// `
