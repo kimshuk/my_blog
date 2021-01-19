68da45e9f6ad5a282d96cffca030e7c125bc576a
Andrew Kim <jeesoo2002@gmail.com>
2021-01-19 21:34:24 +0900

    Post comp styling done

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -5,7 +5,24 @@ import Link from '../Link/Link';
 
 const useStyles = makeStyles({
   button: {
-    backgroundColor: '#fafafa'
+    backgroundColor: '#fafafa',
+    boxShadow: 'none',
+    border: '1px solid #e6e6e6',
+    position: 'fixed',
+    margin: '0',
+    top: '30px',
+    left: '30px',
+    '&:hover': {
+      backgroundColor: '#fafafa'
+    }
+  },
+  postTitle: {
+    textAlign: 'center',
+    fontWeight: '600',
+    fontSize: '3rem'
+  },
+  publishedText: {
+    fontStyle: 'italic'
   }
 })
 
@@ -17,12 +34,12 @@ const Post = ({post}) => {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
-          <Fab className={classes.button} href="/" variant="extended">
-            Go to the main page
+          <Fab className={classes.button} href="/" variant="extended" size="medium">
+            All Articles
           </Fab>
-          <Typography variant="h2" component="h1" gutterBottom>{title}</Typography>
+          <Typography className={classes.postTitle} variant="h2" component="h1" gutterBottom>{title}</Typography>
           <div dangerouslySetInnerHTML={{__html: html}} />
-          <Typography variant="body1" component="p">Published on: {new Date(date).toLocaleDateString('ko-KR', { year: 'numeric', month: 'short', day: 'numeric' })}</Typography>
+          <Typography className={classes.publishedText} variant="body1" component="p">Published on: {new Date(date).toLocaleDateString('ko-KR', { year: 'numeric', month: 'short', day: 'numeric' })}</Typography>
       </Box>
     </Container>
   )
